92a0265eb0a895c17d1a59becc2227a0c8212570
RMDarthMaul <sundevilgoalie13@gmail.com>
2016-05-17 13:23:21 -0400

    Update serialization settings to include contract resolver for NHibernate types.

--- AtwoodUtils/SerializationSettings.cs
+++ AtwoodUtils/SerializationSettings.cs
@@ -8,7 +8,21 @@ namespace AtwoodUtils
     {
         public static readonly JsonSerializerSettings StandardSettings = new JsonSerializerSettings
         {
-            Converters = new List<JsonConverter> { new StringEnumConverter { CamelCaseText = false } }
+            Converters = new List<JsonConverter> { new StringEnumConverter { CamelCaseText = false } },
+            ContractResolver = new NHibernateContractResolver(),
+            PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+            ReferenceLoopHandling = ReferenceLoopHandling.Serialize
         };
+
+        public class NHibernateContractResolver : Newtonsoft.Json.Serialization.DefaultContractResolver
+        {
+            protected override Newtonsoft.Json.Serialization.JsonContract CreateContract(System.Type objectType)
+            {
+                if (typeof(NHibernate.Proxy.INHibernateProxy).IsAssignableFrom(objectType))
+                    return base.CreateContract(objectType.BaseType);
+                else
+                    return base.CreateContract(objectType);
+            }
+        }
     }
 }
