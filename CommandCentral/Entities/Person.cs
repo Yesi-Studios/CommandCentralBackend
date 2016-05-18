8d34cff799adef16945d34940a1377fa5d01e39d
RMDarthMaul <sundevilgoalie13@gmail.com>
2016-05-18 14:31:52 -0400

    Removed Correspondences.

--- CommandCentral/Entities/Person.cs
+++ CommandCentral/Entities/Person.cs
@@ -1335,9 +1335,6 @@ namespace CommandCentral.Entities
         /// </summary>
         public class PersonMapping : ClassMap<Person>
         {
-            //TEST
-
-
             /// <summary>
             /// Maps a person to the database.
             /// </summary>
