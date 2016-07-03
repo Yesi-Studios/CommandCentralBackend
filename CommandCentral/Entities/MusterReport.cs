1fe6d832e5b7a5aa4d675558e9ddfc380144c175
Xanneth <Xanneth@DUCHESS>
2016-07-03 19:45:16 -0400

    Added MusterReport as a method for storing statistics information we generate during the roll over of muster.

---
+++ CommandCentral/Entities/MusterReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+
+namespace CommandCentral.Entities
+{
+    /// <summary>
+    /// Defines a single muster report which is the report that goes our when muster has been finalized for a given day.
+    /// <para/>
+    /// We could build this information every time someone asked for it by just querying back over the database but I figure storage is cheaper than CPU time so I may as well store the reports after we make them.
+    /// </summary>
+    public class MusterReport
+    {
+        public virtual Guid Id { get; set; }
+
+        public virtual int MusterDayOfYear { get; set; }
+
+        public virtual int MusterYear { get; set; }
+
+
+
+    }
+}
