c11a4b3c22b6ca46373860d52b982a30122787ce
DanielAtwood <sundevilgoalie13@gmail.com>
2016-07-12 08:46:12 -0400

    Updated Startup Method and comments on it.

--- CommandCentral/ServiceManagement/StartMethodAttribute.cs
+++ CommandCentral/ServiceManagement/StartMethodAttribute.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace CommandCentral.ServiceManagement
 {
+    /// <summary>
+    /// This attribute indicates that the following method should be run when the service launches with the given priority.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
     public class StartMethodAttribute : Attribute
     {
+        /// <summary>
+        /// The priority with which the given method should run.  Higher priorities run first.
+        /// </summary>
         public int Priority { get; set; }
     }
 }
