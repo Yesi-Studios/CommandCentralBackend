8555a8643f5b10541528e263d8b1d811b8b25e7d
DanielAtwood <sundevilgoalie13@gmail.com>
2016-07-20 08:34:41 -0400

    Added remote administration endpoints for endpoints themselves.  GetEndpoints and SwitchEndpoint.

--- CommandCentral/ServiceManagement/ServiceManager.cs
+++ CommandCentral/ServiceManagement/ServiceManager.cs
@@ -40,6 +40,8 @@ namespace CommandCentral.ServiceManagement
 
         }
 
+        #region Setup Methods
+
         /// <summary>
         /// Scans the entire executing assembly for any service endpoint methods and reads them into a dictionary.
         /// </summary>
@@ -133,5 +135,96 @@ namespace CommandCentral.ServiceManagement
                 Communicator.PostMessageToHost("Executed startup method {0} with priority {1}.".FormatS(info.Name, info.Priority), Communicator.MessageTypes.Informational);
             }
         }
+
+        #endregion
+
+        #region Client Methods
+
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para />
+        /// Returns all endpoints being served by the service.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [EndpointMethod(EndpointName = "GetEndpoints", AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = true)]
+        private static void EndpointMethod_LoadEndpoints(MessageToken token)
+        {
+            //Just make sure the client is logged in.
+            if (token.AuthenticationSession == null)
+            {
+                token.AddErrorMessage("You must be logged in to manage endpoints.", ErrorTypes.Authentication, System.Net.HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            //You have permission?
+            if (!token.AuthenticationSession.Person.HasSpecialPermissions(Authorization.SpecialPermissions.Developer))
+            {
+                token.AddErrorMessage("You don't have permission to manage endpoints - you must be a developer.", ErrorTypes.Authorization, System.Net.HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            token.SetResult(EndpointDescriptions.Select(x => new 
+                {
+                    x.Value.EndpointMethodAttribute.EndpointName,
+                    x.Value.IsActive
+                }).ToList());
+        }
+
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para />
+        /// Given the name of an endpoint, switches it from active to inactive or vice versa.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [EndpointMethod(EndpointName = "SwitchEndpoint", AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = false)]
+        private static void EndpointMethod_SwitchEndpoint(MessageToken token)
+        {
+            //Just make sure the client is logged in.
+            if (token.AuthenticationSession == null)
+            {
+                token.AddErrorMessage("You must be logged in to manage endpoints.", ErrorTypes.Authentication, System.Net.HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            //You have permission?
+            if (!token.AuthenticationSession.Person.HasSpecialPermissions(Authorization.SpecialPermissions.Developer))
+            {
+                token.AddErrorMessage("You don't have permission to manage endpoints - you must be a developer.", ErrorTypes.Authorization, System.Net.HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            //Ok, the client has permission to manage endpoints, let's see what endpoint they're talking about.
+            if (!token.Args.ContainsKey("endpoint"))
+            {
+                token.AddErrorMessage("You failed to send an 'endpoint' parameter!", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            string endpoint = token.Args["endpoint"] as string;
+
+            ServiceEndpoint serviceEndpoint;
+            if (!EndpointDescriptions.TryGetValue(endpoint, out serviceEndpoint))
+            {
+                token.AddErrorMessage("The endpoint parameter you sent was not a real endpoint.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            //Since we got a real endpoint let's go ahead and switch it.
+            serviceEndpoint.IsActive = !serviceEndpoint.IsActive;
+
+            //Now give the client the state of all endpoints.
+            token.SetResult(EndpointDescriptions.Select(x => new
+            {
+                x.Value.EndpointMethodAttribute.EndpointName,
+                x.Value.IsActive
+            }).ToList());
+
+            //TODO send an email to the devs informing them of the state change of an endpoint.
+        }
+
+        #endregion
+
     }
 }
