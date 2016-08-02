6fd8cbc3686f0d4485f0bf9aae678eef72954d0a
DanielAtwood <sundevilgoalie13@gmail.com>
2016-08-02 16:57:02 -0400

    Added an enum which enumerates the names of the sub-modules in the main module.  Submodule names are still referred to by strings in the permission groups.

---
+++ CommandCentral/Authorization/SubModulesEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandCentral.Authorization
+{
+    public enum SubModules
+    {
+        CreatePerson,
+        AdminTools,
+        EditNews
+    }
+}
