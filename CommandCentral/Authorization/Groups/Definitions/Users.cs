7a30fe1fab22b8a7baf39ae49ae87b7596b7bb70
DanielAtwood <sundevilgoalie13@gmail.com>
2016-08-02 16:54:44 -0400

    Fleshed out Users permission group.

--- CommandCentral/Authorization/Groups/Definitions/Users.cs
+++ CommandCentral/Authorization/Groups/Definitions/Users.cs
@@ -6,26 +6,42 @@ using System.Threading.Tasks;
 
 namespace CommandCentral.Authorization.Groups.Definitions
 {
+    /// <summary>
+    /// The users permission group.  Default, and assigned to all persons.
+    /// </summary>
     public class Users : PermissionGroup
     {
+        /// <summary>
+        /// The users permission group.  Default, and assigned to all persons.
+        /// </summary>
         public Users()
         {
             Name("Users");
             Default();
 
-            CanAccessSubModules("CreatePerson", "SomethingElse");
+            CanAccessSubModules(new[] { SubModules.EditNews }.Select(x => x.ToString()).ToArray());
             CanEditMembershipOf();
 
             CanAccessModule("Main")
                 .AtLevel(PermissionGroupLevels.Division)
-                .CanEdit(PropertySelector.Properties<Entities.Person, string>(
-                    x => x.SSN,
-                    x => x.FirstName), PropertySelector.Properties<Entities.Person, DateTime?>(
-                    x => x.DateOfBirth))
-                    .IfInChainOfCommand()
-                .And.
-
-
+                .CanReturn(PropertySelector.Properties<Entities.Person, Guid>(
+                    x => x.Id), PropertySelector.Properties<Entities.Person, string>(
+                    x => x.LastName,
+                    x => x.FirstName,
+                    x => x.MiddleName,
+                    x => x.Suffix,
+                    x => x.Remarks,
+                    x => x.Supervisor,
+                    x => x.WorkCenter,
+                    x => x.WorkRoom))
+                .And.CanReturn(PropertySelector.Properties<Entities.Person, string>(
+                    x => x.SSN))
+                    .IfSelf()
+                .And.CanEdit(PropertySelector.Properties<Entities.Person, string>(
+                    x => x.FirstName,
+                    x => x.LastName,
+                    x => x.MiddleName))
+                    .IfSelf();
         }
     }
 }
