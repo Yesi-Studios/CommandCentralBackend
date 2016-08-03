82b1be468282b7bce8981f21d19cd6b76ab0d41d
Xanneth <Xanneth@DUCHESS>
2016-08-03 01:42:35 -0400

    Implementing the resolve method.  Created extensions for a list of permission groups to make working with them easier.

---
+++ CommandCentral/Authorization/AuthorizationExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AtwoodUtils;
+
+namespace CommandCentral.Authorization
+{
+    /// <summary>
+    /// Extensions that help with authorization stuff.  Mostly operating on multiple permission groups.
+    /// </summary>
+    public static class AuthorizationExtensions
+    {
+        /// <summary>
+        /// Returns a boolean indicating if the given permission groups allow a person to access all of the given submodules.
+        /// <para />
+        /// Case insensitive.
+        /// </summary>
+        /// <param name="groups"></param>
+        /// <param name="submodules"></param>
+        /// <returns></returns>
+        public static bool CanAccessSubmodules(this IEnumerable<Groups.PermissionGroup> groups, params string[] submodules)
+        {
+            return groups.SelectMany(x => x.AccessibleSubModules).Intersect(submodules, StringComparer.CurrentCultureIgnoreCase).Any();
+        }
+
+        /// <summary>
+        /// Resolves a list of permission groups into a resolved permission for the give person.
+        /// </summary>
+        /// <param name="person"></param>
+        /// <param name="client"></param>
+        /// <param name="groups"></param>
+        /// <returns></returns>
+        public static ResolvedPermissions Resolve(this List<Groups.PermissionGroup> groups, Entities.Person client, Entities.Person person)
+        {
+            var resolvedPermissions = new ResolvedPermissions();
+            resolvedPermissions.TimeResolved = DateTime.Now;
+            resolvedPermissions.ClientId = client.Id;
+            resolvedPermissions.PersonId = person.Id;
+
+            //Now we need to start iterating.
+            foreach (var group in groups)
+            {
+                //Add the editable permission groups.
+                foreach (var editPermissionGroup in group.GroupsCanEditMembershipOf)
+                    if (!resolvedPermissions.EditablePermissionGroups.Contains(editPermissionGroup.GroupName))
+                        resolvedPermissions.EditablePermissionGroups.Add(editPermissionGroup.GroupName);
+
+                //And the accessible submodules.
+                foreach (var accessibleSubmodule in group.AccessibleSubModules)
+                    if (!resolvedPermissions.AccessibleSubmodules.Contains(accessibleSubmodule))
+                        resolvedPermissions.AccessibleSubmodules.Add(accessibleSubmodule);
+
+                foreach (var module in group.Modules)
+                {
+                    //First the highest level.
+                    if (!resolvedPermissions.HighestLevels.ContainsKey(module.ModuleName))
+                        resolvedPermissions.HighestLevels.Add(module.ModuleName, module.Level);
+                    else
+                        if (resolvedPermissions.HighestLevels[module.ModuleName] < module.Level)
+                            resolvedPermissions.HighestLevels[module.ModuleName] = module.Level;
+
+                    //And now for editable fields.
+
+
+
+                }
+
+            }
+
+            
+
+
+
+            return resolvedPermissions;
+        }
+    }
+}
---
+++ CommandCentral/Authorization/ResolvedPermissions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandCentral.Authorization
+{
+    /// <summary>
+    /// Defines a class that stores a permission group after it has been refined.
+    /// </summary>
+    public class ResolvedPermissions
+    {
+        /// <summary>
+        /// The list of permission groups' names that went into this resolved permission.
+        /// </summary>
+        public string PermissionGroupNames { get; set; }
+
+        /// <summary>
+        /// The time at which this resolved permission was built.
+        /// </summary>
+        public DateTime TimeResolved { get; set; }
+
+        /// <summary>
+        /// The Id of the client for whom this resolved permission was made.
+        /// </summary>
+        public Guid ClientId { get; set; }
+
+        /// <summary>
+        /// The person against which these permissions were resolved.
+        /// </summary>
+        public Guid PersonId { get; set; }
+
+        /// <summary>
+        /// The list of editable fields, broken down by what module they belong to.  The key is case insensitive.
+        /// </summary>
+        public Dictionary<string, List<string>> EditableFields { get; set; }
+
+        /// <summary>
+        /// The list of returnable fields, broken down by what module they belong to.  The key is case insensitive.
+        /// </summary>
+        public Dictionary<string, List<string>> ReturnableFields { get; set; }
+
+        /// <summary>
+        /// THe highest levels in each of the modules that this client has.  The key is case insensitive.
+        /// </summary>
+        public Dictionary<string, Groups.PermissionGroupLevels> HighestLevels { get; set; }
+
+        /// <summary>
+        /// The list of those permission groups' names that this client can edit the membership of.
+        /// </summary>
+        public List<string> EditablePermissionGroups { get; set; }
+
+        /// <summary>
+        /// The list of all submodules this client can access.
+        /// </summary>
+        public List<string> AccessibleSubmodules { get; set; }
+
+        /// <summary>
+        /// Creates a new resolved permissions.
+        /// </summary>
+        public ResolvedPermissions()
+        {
+            AccessibleSubmodules = new List<string>();
+            EditableFields = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
+            ReturnableFields = new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase);
+            HighestLevels = new Dictionary<string, Groups.PermissionGroupLevels>(StringComparer.OrdinalIgnoreCase);
+            EditablePermissionGroups = new List<string>();
+        }
+
+    }
+}
