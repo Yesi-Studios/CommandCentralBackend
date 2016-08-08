dd1990f21819563eedb0b418d7ed5edfaff23526
DanielAtwood <sundevilgoalie13@gmail.com>
2016-08-08 15:14:10 -0400

    In startup method, the NHibernateHelper will now scan the database premptively.  If issues are found, it'll try to create the schema and everything in the database before continuing.

--- CommandCentral/DataAccess/NHibernateHelper.cs
+++ CommandCentral/DataAccess/NHibernateHelper.cs
@@ -9,6 +9,7 @@ using NHibernate.Caches.SysCache;
 using NHibernate.Cfg;
 using NHibernate.Metadata;
 using System.Linq;
+using AtwoodUtils;
 
 namespace CommandCentral.DataAccess
 {
@@ -24,6 +25,8 @@ namespace CommandCentral.DataAccess
 
         private static ConcurrentDictionary<string, IClassMetadata> _allClassMetadata;
 
+        private static Configuration config = null;
+
         /// <summary>
         /// Indicates that the NHibernateHelper has been initialized.
         /// </summary>
@@ -36,11 +39,9 @@ namespace CommandCentral.DataAccess
         public static void InitializeNHibernate(ConnectionSettings settings)
         {
 
-            Configuration configuration = null;
-            
             if (settings.VerboseLogging)
             {
-                configuration = Fluently.Configure().Database(
+                config = Fluently.Configure().Database(
                 MySQLConfiguration.Standard.ConnectionString(
                     builder => builder.Database(settings.Database)
                         .Username(settings.Username)
@@ -54,7 +55,7 @@ namespace CommandCentral.DataAccess
             }
             else
             {
-                configuration = Fluently.Configure().Database(
+                config = Fluently.Configure().Database(
                 MySQLConfiguration.Standard.ConnectionString(
                     builder => builder.Database(settings.Database)
                         .Username(settings.Username)
@@ -67,9 +68,9 @@ namespace CommandCentral.DataAccess
             }
 
             //We're going to save the schema in case the host wants to use it later.
-            _schema = new NHibernate.Tool.hbm2ddl.SchemaExport(configuration);
+            _schema = new NHibernate.Tool.hbm2ddl.SchemaExport(config);
 
-            _sessionFactory = configuration.BuildSessionFactory();
+            _sessionFactory = config.BuildSessionFactory();
 
             _allClassMetadata = new ConcurrentDictionary<string, IClassMetadata>(
                 _sessionFactory.GetAllClassMetadata()
@@ -84,6 +85,8 @@ namespace CommandCentral.DataAccess
 
             IsInitialized = true;
         }
+
+        #region Helper Methods
 
         /// <summary>
         /// Creates a new session from the session factory.
@@ -109,6 +112,12 @@ namespace CommandCentral.DataAccess
             return _sessionFactory.OpenStatelessSession();
         }
 
+        /// <summary>
+        /// Gets the Id for a given object.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="identifierPropertyName"></param>
+        /// <returns></returns>
         public static object GetIdentifier(object entity, string identifierPropertyName = "Id")
         {
             return entity.GetType().GetProperty(identifierPropertyName).GetValue(entity);
@@ -166,5 +175,119 @@ namespace CommandCentral.DataAccess
 
             _schema.Create(writer, true);
         }
+
+        #endregion
+
+        #region Startup Methods
+
+        /// <summary>
+        /// We're going back to our roots here.  This method, using native sql, will determine if the database that NHIbernate is expecting actually exists.
+        /// <para />
+        /// If it doesn't, we'll make it.  Then since we just had to make it, we'll then run the schema generation script.
+        /// </summary>
+        [ServiceManagement.StartMethod(Priority = 100)]
+        private static void ConfirmDatabaseIntegrity()
+        {
+            var currentSettings = DataAccess.ConnectionSettings.PredefinedConnectionSettings[DataAccess.ConnectionSettings.CurrentSettingsKey];
+
+            Communicator.PostMessage("Beginning database integrity check...", Communicator.MessageTypes.Informational);
+
+            //First, we need to ping the database and make sure it's replying.
+            Communicator.PostMessage("Confirming connection to database : '{0}'...".FormatS(currentSettings.Server), Communicator.MessageTypes.Informational);
+            try
+            {
+                var connectionString = String.Format("server={0};uid={1};pwd={2}", currentSettings.Server, currentSettings.Username, currentSettings.Password);
+
+                using (MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    Communicator.PostMessage("Database connection established.", Communicator.MessageTypes.Informational);
+
+                    //Ok, the connection to the database is good.  Now let's see if the schema is valid.
+                    Communicator.PostMessage("Confirming schema...", Communicator.MessageTypes.Informational);
+
+                    using (MySql.Data.MySqlClient.MySqlCommand command = 
+                        new MySql.Data.MySqlClient.MySqlCommand("SELECT COUNT(SCHEMA_NAME) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = @schema", connection))
+                    {
+                        command.Parameters.AddWithValue("@schema", currentSettings.Database);
+
+                        var exists = ((int)command.ExecuteScalar()) != 0;
+
+                        if (exists)
+                        {
+                            Communicator.PostMessage("Database schema found.", Communicator.MessageTypes.Informational);
+                            Communicator.PostMessage("Scanning for tables...", Communicator.MessageTypes.Informational);
+
+                            List<string> nonexistantTables = new List<string>();
+
+                            //Ok the schema was found, now we need to check to see that all the tables NHibernate expects are there.
+                            //If the tables aren't there, we need to fail.
+                            foreach (var table in config.ClassMappings.Select(x => x.Table))
+                            {
+                                command.Parameters.Clear();
+                                command.CommandText = "SELECT COUNT(*) FROM information_schema.TABLES WHERE TABLE_SCHEMA = @schema AND TABLE_NAME = @table";
+                                command.Parameters.AddWithValue("@schema", currentSettings.Database);
+                                command.Parameters.AddWithValue("@table", table.Name);
+
+                                //Ok is the table there?  If not, add it to a collection.
+                                if (((int)command.ExecuteScalar()) != 0)
+                                    nonexistantTables.Add(table.Name);
+                            }
+
+                            if (nonexistantTables.Any())
+                            {
+                                Communicator.PostMessage("One or more tables were not found in the database that NHibernate expected to exist.  Tables : {0}".FormatS(String.Join(",", nonexistantTables)), Communicator.MessageTypes.Critical);
+                                throw new Exception("One or more tables were not found in the database that NHibernate expected to exist.  Tables : {0}".FormatS(String.Join(",", nonexistantTables)));
+                            }
+                            else
+                            {
+                                Communicator.PostMessage("All tables found.", Communicator.MessageTypes.Informational);
+                            }
+                        }
+                        else
+                        {
+                            Communicator.PostMessage("The database schema, '{0}', was not found.  Creating it now.".FormatS(currentSettings.Database), Communicator.MessageTypes.Warning);
+
+                            //Database not found! Uh oh!
+                            //In this case, we need to make the schema.
+                            command.CommandText = "CREATE DATABASE IF NOT EXISTS @schema";
+
+                            command.ExecuteNonQuery();
+
+                            Communicator.PostMessage("Database schema created.", Communicator.MessageTypes.Warning);
+
+                            //Since the database was just created, let's go ahead and populate it.
+                            CreateSchema(true);
+                        }
+                    }
+                }
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        {
+                            Communicator.PostMessage("Database could not be contacted!  Throwing exception!", Communicator.MessageTypes.Critical);
+                            throw new Exception("The server at '{0}' did not reply.".FormatS(currentSettings.Server));
+                        }
+                    case 1045:
+                        {
+                            Communicator.PostMessage("The Username/password combination for the database was invalid! Throwing exception!", Communicator.MessageTypes.Critical);
+                            throw new Exception("The Username/password combination for the database was invalid");
+                        }
+                    default:
+                        {
+                            Communicator.PostMessage("An unexpected error occured while connecting to the database! Throwing exception!  Error: {0}".FormatS(ex.Message), Communicator.MessageTypes.Critical);
+                            throw ex;
+                        }
+                }
+            }
+        }
+
+        #endregion
+
     }
 }
