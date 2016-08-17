e0a78217faf1d5c7dddc5de6010015d4652ba20a
Xanneth <Xanneth@DUCHESS>
2016-08-17 17:10:11 -0400

    Implemented the fatal error email and moved some stuff into a config file.  Also wrote a new logger - the email logger.

---
+++ CCServ/Config/Email.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCServ.Config
+{
+    public static class Email
+    {
+        public static System.Net.Mail.MailAddress DeveloperDistroAddress
+        {
+            get
+            {
+                return new System.Net.Mail.MailAddress("usn.gordon.inscom.list.nsag-nioc-ga-webmaster@mail.mil", "Command Central Communications");
+            }
+        }
+
+        public static System.Net.Mail.MailAddress AtwoodAddress
+        {
+            get
+            {
+                return new System.Net.Mail.MailAddress("sundevilgoalie13@gmail.com", "Programmer Extraordinaire");
+            }
+        }
+
+        public static System.Net.Mail.MailAddress McLean
+        {
+            get
+            {
+                return new System.Net.Mail.MailAddress("anguslmm@gmail.com", "Angus McLean");
+            }
+        }
+    }
+}
