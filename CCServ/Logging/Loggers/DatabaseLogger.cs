0c24757a92f6dd8e303ed57512d80c60a898fd89
DanielAtwood <sundevilgoalie13@gmail.com>
2016-08-26 11:38:53 -0400

    Added database logger.

---
+++ CCServ/Logging/Loggers/DatabaseLogger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCServ.Logging.Loggers
+{
+    public class DatabaseLogger : ILogger
+    {
+        public DatabaseLogger()
+        {
+        }
+
+        public List<MessageTypes> EnabledMessageTypes { get; set; }
+
+        public LoggingTargetTypes TargetType
+        {
+            get 
+            { 
+                return LoggingTargetTypes.DATABASE; 
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "CCSERV Database Logger NOT IMPLEMENTED";
+            }
+        }
+
+        public bool IsValid()
+        {
+            return true;
+        }
+
+        public void LogDebug(string message, ClientAccess.MessageToken token, string callerMemberName, int callerLineNumber, string callerFilePath)
+        {
+            //TODO
+        }
+
+        public void LogInformation(string message, ClientAccess.MessageToken token, string callerMemberName, int callerLineNumber, string callerFilePath)
+        {
+            //TODO
+        }
+
+        public void LogCritical(string message, ClientAccess.MessageToken token, string callerMemberName, int callerLineNumber, string callerFilePath)
+        {
+            //TODO
+        }
+
+        public void LogWarning(string message, ClientAccess.MessageToken token, string callerMemberName, int callerLineNumber, string callerFilePath)
+        {
+            //TODO
+        }
+
+        public void LogException(Exception ex, string message, ClientAccess.MessageToken token, string callerMemberName, int callerLineNumber, string callerFilePath)
+        {
+            //TODO
+        }
+    }
+}
