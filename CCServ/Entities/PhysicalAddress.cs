8e9acff7e5a31e704b942eb0afb91745fbdd32dc
Xanneth <Xanneth@DUCHESS>
2016-09-01 00:25:15 -0400

    Implemented geo query searching in the advanced search endpoint.

--- CCServ/Entities/PhysicalAddress.cs
+++ CCServ/Entities/PhysicalAddress.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using AtwoodUtils;
 using FluentNHibernate.Mapping;
 using FluentValidation;
@@ -77,6 +79,69 @@ namespace CCServ.Entities
         {
             if (Id == default(Guid))
                 Id = Guid.NewGuid();
+        }
+
+        #endregion
+
+        #region Client Access Methods
+
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para/>
+        /// Acts as an in between for the US Census and the front end.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [ClientAccess.EndpointMethod(EndpointName = "ResolveAddress", AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = false)]
+        private static void EndpointMethod_ResolveAddress(ClientAccess.MessageToken token)
+        {
+            if (!token.Args.ContainsKeys("address", "city", "state", "zip"))
+            {
+                token.AddErrorMessage("You must send the parameters address, city, state, and zip.", ClientAccess.ErrorTypes.Validation, HttpStatusCode.BadRequest);
+                return;
+            }
+
+            token.SetResult(ResolveAddressAgainstCensusData(
+                token.Args["address"] as string, token.Args["city"] as string, token.Args["state"] as string, token.Args["zip"] as string));
+        }
+
+        #endregion
+
+        #region US Census Interaction
+
+        /// <summary>
+        /// Executes a query to the US Census data and returns its response as a JObject.
+        /// </summary>
+        /// <param name="street"></param>
+        /// <param name="city"></param>
+        /// <param name="state"></param>
+        /// <param name="zip"></param>
+        /// <returns></returns>
+        public static Newtonsoft.Json.Linq.JObject ResolveAddressAgainstCensusData(string street, string city, string state, string zip)
+        {
+            try
+            {
+                //This is the query mask for the geocoding service.
+                string apiQueryMask = @"https://geocoding.geo.census.gov/geocoder/locations/address?street={0}&city={1}&state={2}&zip={3}&benchmark=4&layers=all&format=json";
+
+                //Set up the request.
+                var request = WebRequest.Create(apiQueryMask.FormatS(street, city, state, zip));
+                request.ContentType = "application/json; charset=utf-8";
+
+                //Get the response.
+                var response = (HttpWebResponse)request.GetResponse();
+
+                //Read the response.
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    return reader.ReadToEnd().DeserializeToJObject();
+                }
+            }
+            catch (Exception e)
+            {
+                Logging.Log.Exception(e, "An error occurred while communicating with and/or parsing the data from the US Census geocoding service.");
+                throw;
+            }
         }
 
         #endregion
