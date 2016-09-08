17944b813971aee1946524d05de1983e4f87edbb
DanielAtwood <sundevilgoalie13@gmail.com>
2016-09-08 12:13:24 -0400

    Updated UICs.

--- CCServ/Entities/ReferenceLists/UIC.cs
+++ CCServ/Entities/ReferenceLists/UIC.cs
@@ -1,5 +1,10 @@
-﻿using FluentNHibernate.Mapping;
+﻿using System;
+using System.Collections.Generic;
+using CCServ.ClientAccess;
+using FluentNHibernate.Mapping;
 using FluentValidation;
+using AtwoodUtils;
+using System.Linq;
 
 namespace CCServ.Entities.ReferenceLists
 {
@@ -8,6 +13,128 @@ namespace CCServ.Entities.ReferenceLists
     /// </summary>
     public class UIC : EditableReferenceListItemBase
     {
+        /// <summary>
+        /// Update or insert.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="token"></param>
+        public override void UpdateOrInsert(Newtonsoft.Json.Linq.JToken item, ClientAccess.MessageToken token)
+        {
+            using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            using (var transaction = session.BeginTransaction())
+            {
+                try
+                {
+                    var uic = item.CastJToken<UIC>();
+
+                    //Validate it.
+                    var result = uic.Validate();
+                    if (!result.IsValid)
+                    {
+                        token.AddErrorMessages(result.Errors.Select(x => x.ErrorMessage), ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                        return;
+                    }
+
+                    var uicFromDB = session.Get<UIC>(uic.Id);
+
+                    if (uicFromDB == null)
+                    {
+                        uic.Id = Guid.NewGuid();
+                        session.Save(uic);
+                    }
+                    else
+                    {
+                        session.Merge(uic);
+                    }
+
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="forceDelete"></param>
+        /// <param name="token"></param>
+        public override void Delete(System.Guid id, bool forceDelete, ClientAccess.MessageToken token)
+        {
+            using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            using (var transaction = session.BeginTransaction())
+            {
+                try
+                {
+                    var uic = session.Get<UIC>(id);
+
+                    if (uic == null)
+                    {
+                        token.AddErrorMessage("That uic Id was not valid.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                        return;
+                    }
+
+                    var persons = session.QueryOver<Person>().Where(x => x.UIC == uic).List();
+
+                    if (persons.Any())
+                    {
+                        if (forceDelete)
+                        {
+                            foreach (var person in persons)
+                            {
+                                person.UIC = null;
+
+                                session.Update(person);
+                            }
+
+                            //Now that everything is cleaned up, drop the object.
+                            session.Delete(uic);
+                        }
+                        else
+                        {
+                            //There were references but we can't delete them.
+                            token.AddErrorMessage("We were unable to delete the uic, {0}, because it is referenced on {1} profile(s).".FormatS(uic, persons.Count), ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        session.Delete(uic);
+                    }
+
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Load
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="token"></param>
+        public override void Load(System.Guid id, ClientAccess.MessageToken token)
+        {
+            using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            {
+                if (id != default(Guid))
+                {
+                    token.SetResult(session.Get<UIC>(id));
+                }
+                else
+                {
+                    token.SetResult(session.QueryOver<UIC>().List());
+                }
+            }
+        }
 
         /// <summary>
         /// Returns a validation result which contains the result of validation. lol.
@@ -30,10 +157,8 @@ namespace CCServ.Entities.ReferenceLists
             {
                 Id(x => x.Id).GeneratedBy.Assigned();
 
-                Map(x => x.Value).Not.Nullable().Length(10).Unique();
-                Map(x => x.Description).Nullable().Length(40);
-
-                Cache.ReadWrite();
+                Map(x => x.Value).Not.Nullable().Unique();
+                Map(x => x.Description);
             }
         }
 
@@ -47,10 +172,10 @@ namespace CCServ.Entities.ReferenceLists
             /// </summary>
             public UICValidator()
             {
-                RuleFor(x => x.Description).Length(0, 40)
-                    .WithMessage("The description of a UIC must be no more than 40 characters.");
-                RuleFor(x => x.Value).Length(1, 10)
-                    .WithMessage("The value of a UIC must be between one and ten characters.");
+                RuleFor(x => x.Description).Length(0, 255)
+                    .WithMessage("The description of a UIC must be no more than 255 characters.");
+                RuleFor(x => x.Value).NotEmpty()
+                    .WithMessage("The value must not be null.");
             }
         }
 
