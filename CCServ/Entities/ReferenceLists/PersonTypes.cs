fbd234472160fef0a2356af0f221b5306b406646
DanielAtwood <sundevilgoalie13@gmail.com>
2016-09-08 11:47:39 -0400

    Updated person types.

--- CCServ/Entities/ReferenceLists/PersonTypes.cs
+++ CCServ/Entities/ReferenceLists/PersonTypes.cs
@@ -25,28 +25,28 @@ namespace CCServ.Entities.ReferenceLists
         public static ConcurrentBag<PersonType> AllPersonTypes;
 
         public static PersonType Civilian = new PersonType { Id = Guid.Parse("{681624A2-10FD-4124-8D7D-BB854EA9C5B5}"), Value = "Civilian", Description = "" };
-        public static PersonType Civilian = new PersonType { Id = Guid.Parse("{681624A2-10FD-4124-8D7D-BB854EA9C5B5}"), Value = "Civilian", Description = "" };
+        public static PersonType Military = new PersonType { Id = Guid.Parse("{7D8B76CB-ACBD-4269-86D2-751DECAA106E}"), Value = "Military", Description = "" };
 
         /// <summary>
-        /// Ensures that all muster statuses are persisted in the database and that they look the same as they do here.
+        /// Ensures that all person types are persisted in the database and that they look the same as they do here.
         /// </summary>
         /// <param name="options"></param>
-        [ServiceManagement.StartMethod(Priority = 10)]
+        [ServiceManagement.StartMethod(Priority = 12)]
         private static void EnsureMusterStatusesPersistence(CLI.Options.LaunchOptions options)
         {
-            Logging.Log.Info("Checking muster statuses...");
+            Logging.Log.Info("Checking person types...");
 
             using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
             using (var transaction = session.BeginTransaction())
             {
-                var current = session.QueryOver<MusterStatus>().List();
+                var current = session.QueryOver<PersonType>().List();
 
-                var missing = AllMusterStatuses.Except(current).ToList();
+                var missing = AllPersonTypes.Except(current).ToList();
 
-                Logging.Log.Info("Persisting {0} missing muster statuses(s)...".FormatS(missing.Count));
-                foreach (var status in missing)
+                Logging.Log.Info("Persisting {0} missing person types(s)...".FormatS(missing.Count));
+                foreach (var type in missing)
                 {
-                    session.Save(status);
+                    session.Save(type);
                 }
 
                 transaction.Commit();
