478d2526818358a75ce35e3436fe221b584df35a
Xanneth <Xanneth@DUCHESS>
2016-09-16 08:47:15 -0400

    Updated datetime parsing throughout application.

--- CCServ/Tests/TestEndpoints.cs
+++ CCServ/Tests/TestEndpoints.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CCServ.ClientAccess;
+using AtwoodUtils;
+using System.Globalization;
 
 namespace CCServ.Tests
 {
@@ -48,9 +50,31 @@ namespace CCServ.Tests
         /// <param name="token"></param>
         /// <returns></returns>
         [EndpointMethod(EndpointName = "TestDate", AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = false)]
-        private static void EndpointMethod_GetNavyBirthday(MessageToken token)
+        private static void EndpointMethod_TestDate(MessageToken token)
         {
             token.SetResult(new DateTime(1775, 10, 13));
+        }
+
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para />
+        /// Returns the Navy's birthday.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [EndpointMethod(EndpointName = "TestParseDate", AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = false)]
+        private static void EndpointMethod_TestParseDate(MessageToken token)
+        {
+            if (!token.Args.ContainsKey("date"))
+            {
+                token.AddErrorMessage("you failed to send a date parameter.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            DateTime temp;
+            bool result = Utilities.TryParseDateTime(token.Args["date"] as string, out temp);
+
+            token.SetResult(result);
         }
     }
 }
