43b1490ea58a7264e11369d39748ede021b89660
Xanneth <Xanneth@DUCHESS>
2016-09-16 18:04:59 -0400

    Massively overhauled the query engine.

---
+++ CCServ/DataAccess/QueryResultToken.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Criterion;
+
+namespace CCServ.DataAccess
+{
+    public class QueryResultToken<T>
+    {
+        public QueryOver<T, T> Query { get; set; }
+
+        public List<MemberInfo> MembersToSearch { get; set; }
+
+        public List<string> Errors { get; set; }
+
+        public bool HasErrors
+        {
+            get
+            {
+                return Errors.Any();
+            }
+        }
+
+        public QueryResultToken()
+        {
+            Errors = new List<string>();
+            MembersToSearch = new List<MemberInfo>();
+
+        }
+    }
+}
