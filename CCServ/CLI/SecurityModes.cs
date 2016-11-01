96a773b4529c5a3fd9c77b0873caf05dbf6369db
Xanneth <Xanneth@DUCHESS>
2016-11-01 12:21:18 -0400

    Implemented security modes.

---
+++ CCServ/CLI/SecurityModes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCServ.CLI
+{
+    public enum SecurityModes
+    {
+        None,
+        HTTPOnly,
+        DBOnly,
+        Both
+    }
+}
