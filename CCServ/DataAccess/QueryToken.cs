dc5db630fa8f7d6cb44ee023e03230cb8d8a7f2c
Xanneth <Xanneth@DUCHESS>
2016-11-17 01:17:05 -0500

    Added Aliases collection to the query token.

--- CCServ/DataAccess/QueryToken.cs
+++ CCServ/DataAccess/QueryToken.cs
@@ -9,11 +9,28 @@ using NHibernate.Criterion;
 
 namespace CCServ.DataAccess
 {
+    /// <summary>
+    /// Stores the current state of a query as it is built before being submitted to the database.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class QueryToken<T>
     {
+        /// <summary>
+        /// The underlying query, detached from a session.
+        /// </summary>
         public QueryOver<T, T> Query { get; set; }
 
+        /// <summary>
+        /// Contains references to all those aliases that have been used by the application.  Required plumbing for NHibernate.
+        /// 
+        /// The key is the name of the type, while the object is the type itself.
+        /// </summary>
+        public Dictionary<Guid, object> Aliases { get; set; }
+
         private KeyValuePair<MemberInfo, object> _searchParameter;
+        /// <summary>
+        /// The search parameter.
+        /// </summary>
         public KeyValuePair<MemberInfo, object> SearchParameter
         {
             get
@@ -31,8 +48,14 @@ namespace CCServ.DataAccess
             }
         }
 
+        /// <summary>
+        /// Any errors that may have occurred.
+        /// </summary>
         public List<string> Errors { get; set; }
 
+        /// <summary>
+        /// Are there any errors?
+        /// </summary>
         public bool HasErrors
         {
             get
@@ -41,9 +64,13 @@ namespace CCServ.DataAccess
             }
         }
 
+        /// <summary>
+        /// Creates a new query token, initializing the collections.
+        /// </summary>
         public QueryToken()
         {
             Errors = new List<string>();
+            Aliases = new Dictionary<Guid, object>();
         }
     }
 }
