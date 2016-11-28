bfe4cec9984c51aa5957c808365c4993787d3805
DanielAtwood <sundevilgoalie13@gmail.com>
2016-11-28 10:23:50 -0500

    Updated with null check.

--- CCServ/ChangeEventSystem/ChangeEventBase.cs
+++ CCServ/ChangeEventSystem/ChangeEventBase.cs
@@ -34,7 +34,7 @@ namespace CCServ.ChangeEventSystem
         /// <summary>
         /// Given a person, determines all those people who can listen to this event.
         /// </summary>
-        /// <param name="person">This is the person to whom the change occurred.</param>
+        /// <param name="person">This is the person to whom the change occurred. If null, it means a system event was raised NOT regarding a specific person.</param>
         /// <returns></returns>
         public List<System.Net.Mail.MailAddress> GetValidSubscriptionEmailAddresses(Person person)
         {
@@ -68,28 +68,32 @@ namespace CCServ.ChangeEventSystem
                                            .Fetch(x => x.EmailAddresses).Eager
                                            .List<Person>();
 
-                    //Now, we need to select out only those people whose subscription levels match.
-                    //So, if the person subscribed at the division level, but the person in question is only common at the department level, throw out the subscibers.
-                    var finalSubscribers = subscribers.Where(subscriber =>
-                        {
-                            //Here we're going to get the first event whose name matches this event and of those, the highest level.
-                            var subscriptionEvent = subscriber.SubscribedEvents.FirstOrDefault(y => y.ChangeEventName.SafeEquals(this.Name) &&
-                                (y.ChainOfCommandLevel == ChainOfCommandLevels.Command ||
-                                 y.ChainOfCommandLevel == ChainOfCommandLevels.Department ||
-                                 y.ChainOfCommandLevel == ChainOfCommandLevels.Division));
+                    //Make sure the perso isn't null before we ask questions about chain of command.
+                    if (person != null)
+                    {
+                        //Now, we need to select out only those people whose subscription levels match.
+                        //So, if the person subscribed at the division level, but the person in question is only common at the department level, throw out the subscibers.
+                        var finalSubscribers = subscribers.Where(subscriber =>
+                            {
+                                //Here we're going to get the first event whose name matches this event and of those, the highest level.
+                                var subscriptionEvent = subscriber.SubscribedEvents.FirstOrDefault(y => y.ChangeEventName.SafeEquals(this.Name) &&
+                                    (y.ChainOfCommandLevel == ChainOfCommandLevels.Command ||
+                                     y.ChainOfCommandLevel == ChainOfCommandLevels.Department ||
+                                     y.ChainOfCommandLevel == ChainOfCommandLevels.Division));
 
-                            //Ok now that we have that, we're going to ask about the levels and about the subscriber's level.
-                            if (subscriptionEvent.ChainOfCommandLevel == ChainOfCommandLevels.Command)
-                                return subscriber.IsInSameCommandAs(person);
+                                //Ok now that we have that, we're going to ask about the levels and about the subscriber's level.
+                                if (subscriptionEvent.ChainOfCommandLevel == ChainOfCommandLevels.Command)
+                                    return subscriber.IsInSameCommandAs(person);
 
-                            if (subscriptionEvent.ChainOfCommandLevel == ChainOfCommandLevels.Department)
-                                return subscriber.IsInSameDepartmentAs(person);
+                                if (subscriptionEvent.ChainOfCommandLevel == ChainOfCommandLevels.Department)
+                                    return subscriber.IsInSameDepartmentAs(person);
 
-                            if (subscriptionEvent.ChainOfCommandLevel == ChainOfCommandLevels.Division)
-                                return subscriber.IsInSameDivisionAs(person);
+                                if (subscriptionEvent.ChainOfCommandLevel == ChainOfCommandLevels.Division)
+                                    return subscriber.IsInSameDivisionAs(person);
 
-                            throw new Exception("Whole processing the change event, '{0}', we found a subscription to that event with an invalid level: '{1}'.".FormatS(this.Name, subscriptionEvent.ChainOfCommandLevel));
-                        });
+                                throw new Exception("Whole processing the change event, '{0}', we found a subscription to that event with an invalid level: '{1}'.".FormatS(this.Name, subscriptionEvent.ChainOfCommandLevel));
+                            });
+                    }
 
                     //Here are all the email addresses of all the people.
                     result = finalSubscribers.SelectMany(x => x.EmailAddresses.Where(y => y.IsPreferred)
