785a3d6dbee1f847c9b6c855e7dc11490a70731f
Xanneth <Xanneth@DUCHESS>
2016-12-14 13:40:27 -0500

    Implemented create FAQ.

--- CCServ/ClientAccess/Endpoints/FAQEndpoints.cs
+++ CCServ/ClientAccess/Endpoints/FAQEndpoints.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AtwoodUtils;
+using CCServ.Authorization;
 using CCServ.Entities;
+using Humanizer;
+using NHibernate.Criterion;
 
 namespace CCServ.ClientAccess.Endpoints
 {
@@ -82,6 +86,91 @@ namespace CCServ.ClientAccess.Endpoints
                     var faqs = session.QueryOver<FAQ>();
 
                     token.SetResult(faqs);
+
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para />
+        /// Creates an FAQ.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [EndpointMethod(EndpointName = "CreateFAQ", AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = true)]
+        private static void EndpointMethod_CreateFAQ(MessageToken token)
+        {
+            //Just make sure the client is logged in.  The endpoint's description should've handled this but you never know.
+            if (token.AuthenticationSession == null)
+            {
+                token.AddErrorMessage("You must be logged in to create an FAQ.", ErrorTypes.Authentication, System.Net.HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            //Make sure the client has permission to manage the FAQ.
+            if (!token.AuthenticationSession.Person.PermissionGroups.CanAccessSubmodules(SubModules.EditFAQ.ToString()))
+            {
+                token.AddErrorMessage("You do not have permission to manage the FAQ.", ErrorTypes.Authorization, System.Net.HttpStatusCode.Unauthorized);
+            }
+
+            if (!token.Args.ContainsKey("faq"))
+            {
+                token.AddErrorMessage("You failed to send an 'faq' parameter.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            FAQ faq;
+
+            try
+            {
+                faq = token.Args["faq"].CastJToken<FAQ>();
+            }
+            catch (Exception e)
+            {
+                token.AddErrorMessage("There was an error while trying to parse the FAQ you sent.  Error: {0}".FormatWith(e.Message), ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            //Ok now we know we have the FAQ.  Now let's see if it's valid.
+            var validationResult = new FAQ.FAQValidator().Validate(faq);
+
+            if (!validationResult.IsValid)
+            {
+                token.AddErrorMessages(validationResult.Errors.Select(x => x.ErrorMessage), ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            //Well it looks like the faq is valid.  Now we need to reset the Id and then go into a sesion to test the uniqueness of the name and the question.
+            faq.Id = Guid.NewGuid();
+
+            //We passed validation, let's get a sesssion and do ze work.
+            using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            using (var transaction = session.BeginTransaction())
+            {
+                try
+                {
+                    //First, let's make sure the name and the question are both unique.
+                    var results = session.QueryOver<FAQ>().Where(x => x.Name.IsInsensitiveLike(faq.Name) || x.Question.IsInsensitiveLike(faq.Question)).RowCount();
+
+                    if (results != 0)
+                    {
+                        token.AddErrorMessage("That name or that question have already been used.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                        return;
+                    }
+
+                    //Cool!  We know that we have a unique thing now.
+                    //We've already reset the Id, so from here, we're good to insert it.
+                    session.Save(faq);
+
+                    //Finally, send the Id back to the client.
+                    token.SetResult(faq.Id);
 
                     transaction.Commit();
                 }
