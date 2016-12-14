896280094f652b66d9f0d6f31ddcfc1fa36d26b2
Xanneth <Xanneth@DUCHESS>
2016-12-14 13:19:07 -0500

    implemented LoadFAQs.

--- CCServ/ClientAccess/Endpoints/FAQEndpoints.cs
+++ CCServ/ClientAccess/Endpoints/FAQEndpoints.cs
@@ -42,12 +42,54 @@ namespace CCServ.ClientAccess.Endpoints
 
             //We passed validation, let's get a sesssion and do ze work.
             using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            using (var transaction = session.BeginTransaction())
             {
-                //Ok, well it's a GUID.   Do we have it in the database?...
-                var faq = session.Get<FAQ>(faqId);
+                try
+                {
 
-                //Unlike News Items, we don't need a DTO cause we don't have any transformations to do.  We'll just hand back exactly what we loaded.
-                token.SetResult(faq);
+                    //Ok, well it's a GUID.   Do we have it in the database?...
+                    var faq = session.Get<FAQ>(faqId);
+
+                    //Unlike News Items, we don't need a DTO cause we don't have any transformations to do.  We'll just hand back exactly what we loaded.
+                    token.SetResult(faq);
+
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para />
+        /// Loads all FAQs.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [EndpointMethod(EndpointName = "LoadFAQs", AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = false)]
+        private static void EndpointMethod_LoadFAQs(MessageToken token)
+        {
+            //We passed validation, let's get a sesssion and do ze work.
+            using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            using (var transaction = session.BeginTransaction())
+            {
+                try
+                {
+                    var faqs = session.QueryOver<FAQ>();
+
+                    token.SetResult(faqs);
+
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
             }
         }
     }
