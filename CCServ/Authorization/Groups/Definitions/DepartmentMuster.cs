77688feda0f988a06c9e8c8c867aa852ff4802ab
Xanneth <Xanneth@DUCHESS>
2016-12-21 12:27:39 -0500

    Update the comments on the department muster group.

--- CCServ/Authorization/Groups/Definitions/DepartmentMuster.cs
+++ CCServ/Authorization/Groups/Definitions/DepartmentMuster.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace CCServ.Authorization.Groups.Definitions
 {
+    /// <summary>
+    /// Defines the department level group that can muster personnel and nothing else.
+    /// </summary>
     public class DepartmentMuster : PermissionGroup
     {
+        /// <summary>
+        /// Defines the department level group that can muster personnel and nothing else.
+        /// </summary>
         public DepartmentMuster()
         {
             CanEditMembershipOf(typeof(DepartmentMuster), typeof(DivisionMuster));
