e8f81595b560f829580f7148baf33ce88b601094
Xanneth <Xanneth@DUCHESS>
2017-02-16 13:46:23 -0500

    Added Assignments and the comment base.

---
+++ CCServ/Entities/CommentBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCServ.Entities
+{
+    /// <summary>
+    /// The base class for a comment.  Should be implemented by any object that intends to be a "comment" section.
+    /// </summary>
+    public class CommentBase
+    {
+        #region Properties
+
+        /// <summary>
+        /// The unique Id of this comment.
+        /// </summary>
+        public Guid Id { get; set; }
+
+
+
+
+        #endregion
+    }
+}
---
+++ CCServ/Entities/TrainingModule/Assignment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCServ.Entities.TrainingModule
+{
+    /// <summary>
+    /// Defines an assignment, which is how a requirement gets assigned to a person.
+    /// </summary>
+    public class Assignment
+    {
+        /// <summary>
+        /// Unique Id of this assignment.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The date/time that this assignment was created.
+        /// </summary>
+        public DateTime DateAssigned { get; set; }
+
+        
+        
+
+    }
+}
