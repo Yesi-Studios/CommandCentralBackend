3d6c18b8b1da9961f7b8aa596d3e51926a3f34a6
Xanneth <Xanneth@DUCHESS>
2017-02-16 23:10:31 -0500

    Added create requirement endpoint.

--- CCServ/ClientAccess/Endpoints/TrainingModuleEndpoints/RequirementEndpoints.cs
+++ CCServ/ClientAccess/Endpoints/TrainingModuleEndpoints/RequirementEndpoints.cs
@@ -8,6 +8,17 @@ namespace CCServ.ClientAccess.Endpoints.TrainingModuleEndpoints
 {
     static class RequirementEndpoints
     {
-        static void EndpointMethod
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para />
+        /// Creates a training requirement.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [EndpointMethod(EndpointName = "CreateRequirement", AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = true)]
+        static void CreateRequirement(MessageToken token)
+        {
+
+        }
     }
 }
