5eab9e6aa117702016dbd119ac085c38afbf866a
Xanneth <Xanneth@DUCHESS>
2017-02-16 20:28:44 -0500

    Added pipeline.

---
+++ CCServ/Entities/TrainingModule/Pipeline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCServ.Entities.TrainingModule
+{
+    /// <summary>
+    /// A pipeline is essentially just a grouping of certain requirements, allowing users to track a group of trainings and their total completion.
+    /// </summary>
+    public class Pipeline
+    {
+    }
+}
