64401d1404d2eb5d11ecfa7f3eecff7c6d641d31
Xanneth <Xanneth@DUCHESS>
2017-02-16 23:52:42 -0500

    Added training po permission.

---
+++ CCServ/Authorization/Groups/Definitions/TrainingPO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCServ.Authorization.Groups.Definitions
+{
+    /// <summary>
+    /// Any other training POs at the command who can do training events.
+    /// </summary>
+    public class TrainingPO : PermissionGroup
+    {
+        /// <summary>
+        /// Any other training POs at the command who can do training events.
+        /// </summary>
+        public TrainingPO()
+        {
+            CanAccessSubModules(SubModules.ManageTrainingEvents);
+
+            CanEditMembershipOf(typeof(TrainingCoordinator));
+
+            HasAccessLevel(ChainOfCommandLevels.Command);
+
+            InChainsOfCommand(ChainsOfCommand.Training);
+
+            CanAccessModule("Training");
+        }
+    }
+}
