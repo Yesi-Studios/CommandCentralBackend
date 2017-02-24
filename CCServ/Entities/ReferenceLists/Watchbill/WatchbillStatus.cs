da17a5f367f53f7262eb7f423e34aa027253f85f
Xanneth <Xanneth@DUCHESS>
2017-02-23 21:12:17 -0500

    Added watchbill statuses and watch assignment states.

---
+++ CCServ/Entities/ReferenceLists/Watchbill/WatchbillStatus.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CCServ.ClientAccess;
+using FluentNHibernate.Mapping;
+
+namespace CCServ.Entities.ReferenceLists.Watchbill
+{
+    /// <summary>
+    /// Defines a single watch bill status, which is used to indicate at which position in the process a watchbill is at.  
+    /// </summary>
+    public class WatchbillStatus : ReferenceListItemBase
+    {
+
+        /// <summary>
+        /// Loads all objects or a single object if given an Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public override List<ReferenceListItemBase> Load(Guid id, MessageToken token)
+        {
+            using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            {
+                if (id == default(Guid))
+                {
+                    return session.QueryOver<WatchbillStatus>()
+                        .Cacheable().CacheMode(NHibernate.CacheMode.Normal)
+                        .List<ReferenceListItemBase>().ToList();
+                }
+                else
+                {
+                    return new[] { (ReferenceListItemBase)session.Get<WatchbillStatus>(id) }.ToList();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Maps this object to the database.
+        /// </summary>
+        public class WatchbillStatusMapping : ClassMap<WatchbillStatus>
+        {
+            /// <summary>
+            /// Maps this object to the database.
+            /// </summary>
+            public WatchbillStatusMapping()
+            {
+                Id(x => x.Id).GeneratedBy.Assigned();
+
+                Map(x => x.Value).Not.Nullable().Unique();
+                Map(x => x.Description);
+
+                Cache.ReadWrite();
+            }
+        }
+
+    }
+}
