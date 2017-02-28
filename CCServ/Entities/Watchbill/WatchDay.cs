1b17c832785472b80c8b571a83e52e1c8a25f2eb
DanielAtwood <sundevilgoalie13@gmail.com>
2017-02-28 14:39:32 -0500

    Added watch shift and watch day.  Changed watchbill.

---
+++ CCServ/Entities/Watchbill/WatchDay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCServ.Entities.Watchbill
+{
+    /// <summary>
+    /// A watchday acts as a collection of watchshifts and also knows its rank among the other watchdays in its parent watchbill.
+    /// </summary>
+    public class WatchDay
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// The unique Id of this watch day.
+        /// </summary>
+        public virtual Guid Id { get; set; }
+
+        /// <summary>
+        /// The watchbill that owns this watchday.
+        /// </summary>
+        public virtual Watchbill Watchbill { get; set; }
+
+        /// <summary>
+        /// The rank of this watchbill in consideration of the other watch days.
+        /// <para />
+        /// No two watch days should share the same rank, but the parent Watchbill is responsible for this enforcement.
+        /// </summary>
+        public virtual int Rank { get; set; }
+
+        /// <summary>
+        /// The collection of watch shifts contained in this watch day.  These represent the actual watches... eg: A shift from 0800-1200.
+        /// </summary>
+        public virtual IList<WatchShift> WatchShifts { get; set; }
+
+        #endregion
+
+    }
+}
