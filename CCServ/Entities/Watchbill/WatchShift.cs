6f0b4e462a4d19dd61c358162dbb23cc6f49ec6b
Xanneth <Xanneth@DUCHESS>
2017-03-06 23:41:46 -0500

    watch shift mapping and validation

--- CCServ/Entities/Watchbill/WatchShift.cs
+++ CCServ/Entities/Watchbill/WatchShift.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using FluentValidation;
+using NHibernate.Type;
+using AtwoodUtils;
 
 namespace CCServ.Entities.Watchbill
 {
@@ -57,6 +61,61 @@ namespace CCServ.Entities.Watchbill
         public virtual ReferenceLists.Watchbill.WatchShiftType ShiftType { get; set; }
 
         #endregion
+
+        /// <summary>
+        /// Maps this object to the database.
+        /// </summary>
+        public class WatchShiftMapping : ClassMap<WatchShift>
+        {
+            /// <summary>
+            /// Maps this object to the database.
+            /// </summary>
+            public WatchShiftMapping()
+            {
+                Id(x => x.Id).GeneratedBy.Guid();
+
+                References(x => x.ShiftType).Not.Nullable();
+
+                HasMany(x => x.WatchAssignments);
+
+                HasManyToMany(x => x.WatchInputs);
+                HasManyToMany(x => x.WatchDays);
+
+                Map(x => x.Title).Not.Nullable();
+                Map(x => x.From).Not.Nullable().CustomType<UtcDateTimeType>();
+                Map(x => x.To).Not.Nullable().CustomType<UtcDateTimeType>();
+            }
+        }
+
+        /// <summary>
+        /// Validates the parent object.
+        /// </summary>
+        public class WatchShiftValidator : AbstractValidator<WatchShift>
+        {
+            /// <summary>
+            /// Validates the parent object.
+            /// </summary>
+            public WatchShiftValidator()
+            {
+                RuleFor(x => x.ShiftType).NotEmpty();
+                RuleFor(x => x.Title).NotEmpty().Length(1, 50);
+
+                RuleFor(x => x.WatchAssignments).SetCollectionValidator(new WatchAssignment.WatchAssignmentValidator());
+                RuleFor(x => x.WatchInputs).SetCollectionValidator(new WatchInput.WatchInputValidator());
+                RuleFor(x => x.WatchDays).SetCollectionValidator(new WatchDay.WatchDayValidator());
+
+                Custom(watchShift =>
+                {
+                    if (watchShift.From >= watchShift.To)
+                        return new FluentValidation.Results.ValidationFailure(PropertySelector.SelectPropertyFrom<WatchShift>(x => x.From).Name, "The watch shift's from and to dates must make sense.  Please.");
+
+                    if (watchShift.To <= watchShift.From)
+                        return new FluentValidation.Results.ValidationFailure(PropertySelector.SelectPropertyFrom<WatchShift>(x => x.To).Name, "The watch shift's from and to dates must make sense.  Please.");
+
+                    return null;
+                });
+            }
+        }
 
     }
 }
