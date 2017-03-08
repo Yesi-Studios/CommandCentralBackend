e28bb33b27168e3694dd48951b35285d3d493116
DanielAtwood <sundevilgoalie13@gmail.com>
2017-03-08 11:28:27 -0500

    Updated permissions system to include quarterdeck watchbill permissions.

---
+++ CCServ/Authorization/Groups/Definitions/DivisionQuarterdeckWatchbill.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AtwoodUtils;
+
+namespace CCServ.Authorization.Groups.Definitions
+{
+    /// <summary>
+    /// The permission group that is responsible for the quarterdeck watchbill at the division level.
+    /// </summary>
+    public class DivisionQuarterdeckWatchbill : PermissionGroup
+    {
+        /// <summary>
+        /// The permission group that is responsible for the quarterdeck watchbill at the division level.
+        /// </summary>
+        public DivisionQuarterdeckWatchbill()
+        {
+            CanEditMembershipOf(typeof(DivisionQuarterdeckWatchbill));
+
+            HasAccessLevel(ChainOfCommandLevels.Division);
+
+            InChainsOfCommand(ChainsOfCommand.QuarterdeckWatchbill);
+
+            CanAccessModule("Watchbill");
+        }
+    }
+}
