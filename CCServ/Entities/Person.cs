b900d27e0f53d40426fc158fdc9f7e85cb40de5b
DanielAtwood <sundevilgoalie13@gmail.com>
2017-03-17 09:00:30 -0400

    Removed the dod Id validation rules.

--- CCServ/Entities/Person.cs
+++ CCServ/Entities/Person.cs
@@ -833,8 +833,6 @@ namespace CCServ.Entities
                     .WithMessage("The suffix must not exceed 40 characters.");
                 RuleFor(x => x.SSN).NotEmpty().Must(x => System.Text.RegularExpressions.Regex.IsMatch(x, @"^(?!\b(\d)\1+-(\d)\1+-(\d)\1+\b)(?!123-45-6789|219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4}$"))
                     .WithMessage("The SSN must be valid and contain only numbers.");
-                RuleFor(x => x.DoDId).Must((person, str) => str.All(Char.IsDigit))
-                    .WithMessage("A DoD Id must be numbers only and be exactly ten digits.");
                 RuleFor(x => x.DateOfBirth).NotEmpty()
                     .WithMessage("The DOB must not be left blank.");
                 RuleFor(x => x.PRD).NotEmpty()
