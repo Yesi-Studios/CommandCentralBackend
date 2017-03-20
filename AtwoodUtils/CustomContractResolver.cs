5c8960c5d2c17b2805682e7707f5caf5a4cc201e
DanielAtwood <sundevilgoalie13@gmail.com>
2017-03-20 10:58:04 -0400

    Changed and commented serialization settings.

---
+++ AtwoodUtils/CustomContractResolver.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtwoodUtils
+{
+    /// <summary>
+    /// Provides custom contract resolution.  This allows us to make broad changes to how serialization occurs across all objects.
+    /// </summary>
+    public class CustomContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            JsonProperty property = base.CreateProperty(member, memberSerialization);
+
+            var atts = property.AttributeProvider.GetAttributes(typeof(ConditionalJsonIgnoreAttribute), true);
+
+            if (!atts.Any())
+                return property;
+
+            property.ShouldSerialize = x => false;
+
+            return property;
+        }
+
+        protected override JsonContract CreateContract(System.Type objectType)
+        {
+            if (typeof(NHibernate.Proxy.INHibernateProxy).IsAssignableFrom(objectType))
+                return base.CreateContract(objectType.BaseType);
+            else
+                return base.CreateContract(objectType);
+        }
+    }
+}
--- AtwoodUtils/SerializationSettings.cs
+++ AtwoodUtils/SerializationSettings.cs
@@ -7,42 +7,23 @@ using Newtonsoft.Json.Serialization;
 
 namespace AtwoodUtils
 {
+    /// <summary>
+    /// Declares global serialization settings which should be used in all serializations.
+    /// </summary>
     public static class SerializationSettings
     {
+        /// <summary>
+        /// The settings themselves.
+        /// </summary>
         public static readonly JsonSerializerSettings StandardSettings = new JsonSerializerSettings
         {
             Converters = new List<JsonConverter> { new StringEnumConverter { CamelCaseText = false } },
-            ContractResolver = new NHibernateContractResolver(),
+            ContractResolver = new CustomContractResolver(),
             PreserveReferencesHandling = PreserveReferencesHandling.All,
             ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
             Formatting = Formatting.Indented,
             DateFormatString = "yyyy-MM-ddTHH:mm:ss.fffZ",
             DateTimeZoneHandling = DateTimeZoneHandling.Utc
         };
-
-        public class NHibernateContractResolver : DefaultContractResolver
-        {
-            protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
-            {
-                JsonProperty property = base.CreateProperty(member, memberSerialization);
-
-                var atts = property.AttributeProvider.GetAttributes(typeof(ConditionalJsonIgnoreAttribute), true);
-
-                if (!atts.Any())
-                    return property;
-
-                property.ShouldSerialize = x => false;
-
-                return property;
-            }
-
-            protected override JsonContract CreateContract(System.Type objectType)
-            {
-                if (typeof(NHibernate.Proxy.INHibernateProxy).IsAssignableFrom(objectType))
-                    return base.CreateContract(objectType.BaseType);
-                else
-                    return base.CreateContract(objectType);
-            }
-        }
     }
 }
