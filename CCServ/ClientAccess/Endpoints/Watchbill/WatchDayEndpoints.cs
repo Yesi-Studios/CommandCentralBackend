7cfbdc2caaf805da083fb158a3a77cc4a60ac2fc
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-03-20 00:26:22 -0400

    Implemented /Create and /Load

---
+++ CCServ/ClientAccess/Endpoints/Watchbill/WatchDayEndpoints.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AtwoodUtils;
+
+namespace CCServ.ClientAccess.Endpoints.Watchbill
+{
+    class WatchDayEndpoints
+    {
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para />
+        /// Loads a watch day.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [EndpointMethod(AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = true)]
+        private static void LoadWatchDay(MessageToken token)
+        {
+            //Just make sure the client is logged in.  The endpoint's description should've handled this but you never know.
+            if (token.AuthenticationSession == null)
+            {
+                token.AddErrorMessage("You must be logged in to do that.", ErrorTypes.Authentication, System.Net.HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            if (!token.Args.ContainsKey("watchdayid"))
+            {
+                token.AddErrorMessage("You failed to send a 'watchdayid' parameter.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            Guid watchDayId;
+            if (!Guid.TryParse(token.Args["watchdayid"] as string, out watchDayId))
+            {
+                token.AddErrorMessage("Your watch day id parameter's format was invalid.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            //Now let's go get the watch day from the database.
+            using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        var watchDayFromDB = session.Get<Entities.Watchbill.WatchDay>(watchDayId);
+
+                        if (watchDayFromDB == null)
+                        {
+                            token.AddErrorMessage("Your watch day's id was not valid.  Please consider creating the watch day first.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                            return;
+                        }
+
+                        token.SetResult(watchDayFromDB);
+
+                        transaction.Commit();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para />
+        /// Creates a watch day.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [EndpointMethod(AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = true)]
+        private static void CreateWatchDay(MessageToken token)
+        {
+            //Just make sure the client is logged in.  The endpoint's description should've handled this but you never know.
+            if (token.AuthenticationSession == null)
+            {
+                token.AddErrorMessage("You must be logged in to do that.", ErrorTypes.Authentication, System.Net.HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            if (!token.Args.ContainsKey("watchday"))
+            {
+                token.AddErrorMessage("You failed to send a 'watchday' parameter.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            Entities.Watchbill.WatchDay watchDayFromClient;
+            try
+            {
+                watchDayFromClient = token.Args["watchday"].CastJToken<Entities.Watchbill.WatchDay>();
+            }
+            catch
+            {
+                token.AddErrorMessage("An error occurred while trying to parse your watch day.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            var watchDayToInsert = new Entities.Watchbill.WatchDay
+            {
+                Date = watchDayFromClient.Date,
+                Id = Guid.NewGuid(),
+                Remarks = watchDayFromClient.Remarks,
+                Watchbill = watchDayFromClient.Watchbill
+            };
+
+            var validationResult = new Entities.Watchbill.WatchDay.WatchDayValidator().Validate(watchDayToInsert);
+
+            if (!validationResult.IsValid)
+            {
+                token.AddErrorMessages(validationResult.Errors.Select(x => x.ErrorMessage), ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+
+                        //Let's get the watchbill the client says this watch day will be assigned to.
+                        var watchbill = session.Get<Entities.Watchbill.Watchbill>(watchDayFromClient.Watchbill.Id);
+
+                        if (!token.AuthenticationSession.Person.PermissionGroups.Any(x => x.ChainsOfCommandMemberOf.Contains(watchbill.ElligibilityGroup.OwningChainOfCommand) && x.AccessLevel == ChainOfCommandLevels.Command))
+                        {
+                            token.AddErrorMessage("You are not allowed to edit the structure of a watchbill tied to that elligibility group.  You must have command level permissions in the related chain of command.", ErrorTypes.Authorization, System.Net.HttpStatusCode.Unauthorized);
+                            return;
+                        }
+
+                        session.Save(watchDayToInsert);
+
+                        token.SetResult(watchDayToInsert);
+
+                        transaction.Commit();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
