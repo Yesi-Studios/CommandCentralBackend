7276a0a7f4ddea33b58f7291d81a0c81a80f5374
DanielAtwood <sundevilgoalie13@gmail.com>
2017-03-20 09:59:15 -0400

    Added watch input endpoints and implemented /LoadWatchInput

---
+++ CCServ/ClientAccess/Endpoints/Watchbill/WatchInputEndpoints.cs
@@ -0,0 +1,74 @@
+﻿using CCServ.Entities.Watchbill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCServ.ClientAccess.Endpoints.Watchbill
+{
+    /// <summary>
+    /// Contains all the endpoints for interacting with watch inputs.
+    /// </summary>
+    static class WatchInputEndpoints
+    {
+
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para />
+        /// Loads a watch input.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [EndpointMethod(AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = true)]
+        private static void LoadWatchInput(MessageToken token)
+        {
+            //Just make sure the client is logged in.  The endpoint's description should've handled this but you never know.
+            if (token.AuthenticationSession == null)
+            {
+                token.AddErrorMessage("You must be logged in to do that.", ErrorTypes.Authentication, System.Net.HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            if (!token.Args.ContainsKey("watchinputid"))
+            {
+                token.AddErrorMessage("You failed to send a 'watchinputid' parameter.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            Guid watchInputId;
+            if (!Guid.TryParse(token.Args["watchinputid"] as string, out watchInputId))
+            {
+                token.AddErrorMessage("Your watch input id parameter's format was invalid.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        var watchInputFromDB = session.Get<WatchInput>(watchInputId);
+
+                        if (watchInputFromDB == null)
+                        {
+                            token.AddErrorMessage("Your watch input's id was not valid.  Please consider creating the watch input first.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                            return;
+                        }
+
+                        token.SetResult(watchInputFromDB);
+
+                        transaction.Commit();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+
+    }
+}
