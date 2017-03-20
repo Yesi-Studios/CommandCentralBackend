0cb8857c65a2d41ea75d767ba66dbf5efde6ee81
Xanneth <Xanneth@DUCHESS>
2017-03-19 23:35:31 -0400

    Implemented conditional json ignore attribute.

---
+++ AtwoodUtils/ConditionalJsonIgnore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtwoodUtils
+{
+    /// <summary>
+    /// Instructs the serializers to ignore this property during serialization but NOT during deserialization
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ConditionalJsonIgnoreAttribute : Attribute
+    {
+
+    }
+}
