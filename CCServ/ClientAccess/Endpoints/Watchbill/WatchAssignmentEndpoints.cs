2c6f6ff5750e8d087d4b1d271898f347ac1b1391
DanielAtwood <sundevilgoalie13@gmail.com>
2017-03-21 09:57:41 -0400

    Implemented /LoadWatchAssignment

---
+++ CCServ/ClientAccess/Endpoints/Watchbill/WatchAssignmentEndpoints.cs
@@ -0,0 +1,72 @@
+﻿using CCServ.Entities.Watchbill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCServ.ClientAccess.Endpoints.Watchbill
+{
+    /// <summary>
+    /// Defines all of the endpoints used to interact with the watch assignment object.
+    /// </summary>
+    static class WatchAssignmentEndpoints
+    {
+        /// <summary>
+        /// WARNING!  THIS METHOD IS EXPOSED TO THE CLIENT AND IS NOT INTENDED FOR INTERNAL USE.  AUTHENTICATION, AUTHORIZATION AND VALIDATION MUST BE HANDLED PRIOR TO DB INTERACTION.
+        /// <para />
+        /// Loads a watch assignment.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [EndpointMethod(AllowArgumentLogging = true, AllowResponseLogging = true, RequiresAuthentication = true)]
+        private static void LoadWatchAssignment(MessageToken token)
+        {
+            //Just make sure the client is logged in.  The endpoint's description should've handled this but you never know.
+            if (token.AuthenticationSession == null)
+            {
+                token.AddErrorMessage("You must be logged in to do that.", ErrorTypes.Authentication, System.Net.HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            if (!token.Args.ContainsKey("watchassignmentid"))
+            {
+                token.AddErrorMessage("You failed to send a 'watchassignmentid' parameter.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            Guid watchAssignmentId;
+            if (!Guid.TryParse(token.Args["watchassignmentid"] as string, out watchAssignmentId))
+            {
+                token.AddErrorMessage("Your watchassignmentid parameter's format was invalid.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                return;
+            }
+
+            using (var session = DataAccess.NHibernateHelper.CreateStatefulSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    try
+                    {
+                        var watchAssignmentFromDB = session.Get<WatchAssignment>(watchAssignmentId);
+
+                        if (watchAssignmentFromDB == null)
+                        {
+                            token.AddErrorMessage("Your watch assignemnt's id was not valid.  Please consider creating it first.", ErrorTypes.Validation, System.Net.HttpStatusCode.BadRequest);
+                            return;
+                        }
+
+                        token.SetResult(watchAssignmentFromDB);
+
+                        transaction.Commit();
+                    }
+                    catch
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
