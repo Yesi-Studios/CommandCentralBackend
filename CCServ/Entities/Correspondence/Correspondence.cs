4d023ab90d51e175b493e26b3fa0217379c0e50a
DanielAtwood <sundevilgoalie13@gmail.com>
2017-04-19 15:45:57 -0400

    Began the correspondence module.

---
+++ CCServ/Entities/Correspondence/Correspondence.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using FluentValidation;
+using CCServ.Entities.ReferenceLists;
+using CCServ.ClientAccess;
+using System.Globalization;
+using AtwoodUtils;
+using NHibernate.Type;
+
+namespace CCServ.Entities.Correspondence
+{
+    /// <summary>
+    /// Describes a single correspondence object.
+    /// </summary>
+    public class Correspondence : ICommentable, IAttachmentParent
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// The unique Id of this correspondence.
+        /// </summary>
+        public virtual Guid Id { get; set; }
+
+        /// <summary>
+        /// The person that created this correspondence.
+        /// </summary>
+        public virtual Person Creator { get; set; }
+
+        /// <summary>
+        /// The person for whom this correspondence was created.
+        /// </summary>
+        public virtual Person InCareOf { get; set; }
+
+        /// <summary>
+        /// The date/time at which this correspondence was created.
+        /// </summary>
+        public virtual DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// The subject like of a correspondence.  For example: BAH Request.
+        /// </summary>
+        public virtual string Subject { get; set; }
+
+        /// <summary>
+        /// The body of the correspondence is a single, large text field the client can use to write whatever they want.
+        /// </summary>
+        public virtual string Body { get; set; }
+
+        /// <summary>
+        /// The "discussion thread" for this correspondence.
+        /// </summary>
+        public virtual IList<Comment> Comments { get; set; }
+
+        /// <summary>
+        /// This integer is the current step number we're on.  Any steps that have this number are considered the current step.
+        /// </summary>
+        public virtual int CurrentStepOrder { get; set; }
+
+        /// <summary>
+        /// The files that have been attached to this correspondence.  Note: This collection only contains references to each attachment.
+        /// <para />
+        /// Each attachment may be loaded from the /LoadAttachment endpoint.
+        /// </summary>
+        public virtual IList<FileAttachment> Attachments { get; set; }
+
+        /// <summary>
+        /// The type of correspondence.
+        /// </summary>
+        public virtual ReferenceLists.Correspondence.CorrespondenceType CorrespondenceType { get; set; }
+
+        /// <summary>
+        /// The steps in this correspondence.
+        /// </summary>
+        public virtual IList<CorrespondenceStep> Steps { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Maps this object to the database.
+        /// </summary>
+        public class CorrespondenceMapping : ClassMap<Correspondence>
+        {
+            /// <summary>
+            /// Maps this object to the database.
+            /// </summary>
+            public CorrespondenceMapping()
+            {
+                Id(x => x.Id).GeneratedBy.Assigned();
+
+                References(x => x.Creator).Not.Nullable();
+                References(x => x.InCareOf).Not.Nullable();
+                References(x => x.CorrespondenceType).Not.Nullable();
+
+                HasMany(x => x.Comments)
+                    .Cascade.AllDeleteOrphan()
+                    .KeyColumn("EntityOwner_id")
+                    .ForeignKeyConstraintName("none");
+
+                HasMany(x => x.Attachments)
+                    .Cascade.AllDeleteOrphan()
+                    .KeyColumn("EntityOwner_id")
+                    .ForeignKeyConstraintName("none");
+
+                HasMany(x => x.Steps).Inverse();
+
+                Map(x => x.Subject).Not.Nullable();
+                Map(x => x.Body).Not.Nullable().CustomType<StringClobType>();
+                Map(x => x.CurrentStepOrder).Not.Nullable();
+
+                Map(x => x.DateCreated).Not.Nullable().CustomType<UtcDateTimeType>();
+            }
+        }
+
+        /// <summary>
+        /// Validates this object.
+        /// </summary>
+        public class CorrespondenceValidator : AbstractValidator<Correspondence>
+        {
+            /// <summary>
+            /// Validates this object.
+            /// </summary>
+            public CorrespondenceValidator()
+            {
+                RuleFor(x => x.Creator).NotEmpty();
+                RuleFor(x => x.InCareOf).NotEmpty();
+                RuleFor(x => x.DateCreated).NotEmpty();
+                RuleFor(x => x.Subject).NotEmpty().Length(1, 255);
+                RuleFor(x => x.Body).NotEmpty().Length(1, 5000);
+                RuleFor(x => x.CurrentStepOrder).GreaterThanOrEqualTo(0);
+
+                RuleFor(x => x.CorrespondenceType).NotEmpty();
+
+                RuleFor(x => x.Comments).SetCollectionValidator(new Comment.CommentValidator());
+
+                //TODO: step validator and attachment validator
+            }
+        }
+
+    }
+}
---
+++ CCServ/Entities/FileAttachment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CCServ.Entities;
+using System.Collections;
+using AtwoodUtils;
+using Newtonsoft.Json;
+
+namespace CCServ.Entities
+{
+    /// <summary>
+    /// Describes a single file attachment.
+    /// </summary>
+    public class FileAttachment
+    {
+        public virtual Guid Id { get; set; }
+
+        [JsonIgnore]
+        public virtual string FilePath { get; set; }
+    }
+}
