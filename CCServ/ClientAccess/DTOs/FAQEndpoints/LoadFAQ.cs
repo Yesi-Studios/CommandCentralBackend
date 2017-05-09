1a40b395a5b605c7ddf927fe02a7bcb2b0f731ac
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-05-09 15:08:18 -0400

    Tried something out that didn't work.  And implemented the faq dto.

---
+++ CCServ/ClientAccess/DTOs/FAQEndpoints/LoadFAQ.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace CCServ.ClientAccess.DTOs.FAQEndpoints
+{
+    /// <summary>
+    /// The dto for the related endpoint.
+    /// </summary>
+    public class LoadFAQ : DTOBase
+    {
+        /// <summary>
+        /// The id of the faq the client wants to load.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Creates a new DTO.
+        /// </summary>
+        /// <param name="obj"></param>
+        public LoadFAQ(JObject obj) : base(obj)
+        {
+        }
+    }
+}
