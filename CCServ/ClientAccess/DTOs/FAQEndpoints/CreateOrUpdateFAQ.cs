844721470b83c4e6f52dc917d80da45b9d30d502
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-05-09 15:46:58 -0400

    work

---
+++ CCServ/ClientAccess/DTOs/FAQEndpoints/CreateOrUpdateFAQ.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using FluentValidation;
+
+namespace CCServ.ClientAccess.DTOs.FAQEndpoints
+{
+    /// <summary>
+    /// The dto for the related endpoint.
+    /// </summary>
+    public class CreateOrUpdateFAQ : DTOBase
+    {
+        /// <summary>
+        /// The faq the client wants to update or create.
+        /// </summary>
+        public Entities.FAQ FAQ { get; set; } 
+        
+        /// <summary>
+        /// Creates a new dto.
+        /// </summary>
+        /// <param name="obj"></param>
+        public CreateOrUpdateFAQ(JObject obj) : base(obj)
+        {
+        }
+
+        class Validator : AbstractValidator<CreateOrUpdateFAQ>
+        {
+
+        }
+    }
+}
