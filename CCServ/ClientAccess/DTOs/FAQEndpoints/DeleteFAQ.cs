1cdd0652437380cf97cc359968c8b69537952efd
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-05-11 10:25:17 -0400

    Completed DTO for FAQ endpoints.

---
+++ CCServ/ClientAccess/DTOs/FAQEndpoints/DeleteFAQ.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace CCServ.ClientAccess.DTOs.FAQEndpoints
+{
+    /// <summary>
+    /// The DTO used for the related endpoint.
+    /// </summary>
+    public class DeleteFAQ : DTOBase
+    {
+
+        /// <summary>
+        /// The Id of the FAQ the client wants to delete.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Creates a new DTO.
+        /// </summary>
+        /// <param name="obj"></param>
+        public DeleteFAQ(JObject obj) : base(obj)
+        {
+        }
+    }
+}
