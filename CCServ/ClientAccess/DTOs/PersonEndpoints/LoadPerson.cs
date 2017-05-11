1f0decf6fb2f4c17b98bbbba4acbce46196e68a6
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-05-11 14:45:28 -0400

    load person dto.

---
+++ CCServ/ClientAccess/DTOs/PersonEndpoints/LoadPerson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace CCServ.ClientAccess.DTOs.PersonEndpoints
+{
+    /// <summary>
+    /// The dto used by the related endpoint.
+    /// </summary>
+    public class LoadPerson : DTOBase
+    {
+
+        /// <summary>
+        /// The id of the person the client wants to load.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Creates a new DTO.
+        /// </summary>
+        /// <param name="obj"></param>
+        public LoadPerson(JObject obj) : base(obj)
+        {
+        }
+    }
+}
