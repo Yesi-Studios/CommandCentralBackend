5967cd1813a047afe453393ed8300006e9ca8233
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-05-21 12:23:59 -0400

    Moved assignment to its own class.

---
+++ CCServ/Assignment.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CCServ.Entities.ReferenceLists;
+
+namespace CCServ
+{
+    /// <summary>
+    /// A wrapper around a division/department/command.
+    /// </summary>
+    public class Assignment
+    {
+        /// <summary>
+        /// The division.
+        /// </summary>
+        public Division Division { get; private set; }
+
+        /// <summary>
+        /// The department.
+        /// </summary>
+        public Department Department { get; private set; }
+
+        /// <summary>
+        /// The command.
+        /// </summary>
+        public Command Command { get; private set; }
+
+        /// <summary>
+        /// Builds a new assignment from arbitrary div/dep/command.
+        /// </summary>
+        /// <param name="div"></param>
+        /// <param name="dep"></param>
+        /// <param name="com"></param>
+        public Assignment(Division div, Department dep, Command com)
+        {
+            this.Division = div;
+            this.Department = dep;
+            this.Command = com;
+        }
+
+        /// <summary>
+        /// Builds an assignment from the div and walks up the line to build the rest.
+        /// </summary>
+        /// <param name="div"></param>
+        public Assignment(Division div)
+        {
+            this.Division = div;
+            this.Department = div?.Department;
+            this.Command = this.Department?.Command;
+        }
+
+    }
+}
