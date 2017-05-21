f07c523eeef69498437449e68d75b763b90ed1d1
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-05-21 12:23:38 -0400

    Created assignment changed event email.

--- CCServ/Email/Models/AssignmentChangedEventEmailModel.cs
+++ CCServ/Email/Models/AssignmentChangedEventEmailModel.cs
@@ -7,49 +7,13 @@ using System.Threading.Tasks;
 namespace CCServ.Email.Models
 {
     /// <summary>
-    /// The email model used for the assignement changed event.
+    /// The email model used for the assignment changed event.
     /// </summary>
     public class AssignmentChangedEventEmailModel
     {
         /// <summary>
-        /// The person's old assignment.
+        /// The change event this email references.
         /// </summary>
-        public AssignmentContainer OldAssignment { get; set; }
-
-        /// <summary>
-        /// The person's new assignment.
-        /// </summary>
-        public AssignmentContainer NewAssignment { get; set; }
-
-        /// <summary>
-        /// The Id of the person whose profile changed.
-        /// </summary>
-        public Guid PersonId { get; set; }
-
-        /// <summary>
-        /// The friendly name of the person whose profile changed.
-        /// </summary>
-        public string FriendlyName { get; set; }
-
-        /// <summary>
-        /// Intended to wrap a single assignemnt which is a division/department/command chain.
-        /// </summary>
-        public class AssignmentContainer
-        {
-            /// <summary>
-            /// Division
-            /// </summary>
-            public string Division { get; set; }
-
-            /// <summary>
-            /// Department
-            /// </summary>
-            public string Department { get; set; }
-
-            /// <summary>
-            /// Command
-            /// </summary>
-            public string Command { get; set; }
-        }
+        public ChangeEventSystem.ChangeEvents.AssignmentChangedEvent ChangeEvent { get; set; }
     }
 }
