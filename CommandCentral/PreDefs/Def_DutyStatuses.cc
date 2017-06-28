477d970e771052718eaaf30880eae992044e6508
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-06-28 11:31:01 -0400

    Added all the defs.

---
+++ CommandCentral/PreDefs/Def_DutyStatuses.cc
@@ -0,0 +1,40 @@
+﻿{
+  "TypeFullName": "CommandCentral.Entities.ReferenceLists.DutyStatus",
+  "Definitions": [
+    {
+      "Id": "C06D1123-60D9-4F06-B4DE-DD15EAE724A2",
+      "Value": "Active",
+      "Description": "Indicates that a person is a member of the Active Duty component of the US armed forces."
+    },
+    {
+      "Id": "CE393F64-6ADB-4CA8-9E76-1691CD473CFC",
+      "Value": "Reserves",
+      "Description": "Indicates that a person is a member of the Reserve component of the US armed forces."
+    },
+    {
+      "Id": "412B00F7-5B33-49D2-B6C4-7F9F2DE4B0CE",
+      "Value": "Contractor",
+      "Description": "Indicates that a person is a contractor."
+    },
+    {
+      "Id": "87B778F8-6373-4A00-902C-10A8AC1207C5",
+      "Value": "Civilian",
+      "Description": "Indicates that a person is a civilian."
+    },
+    {
+      "Id": "63E1584E-47AF-44C2-BEEC-7814C2356489",
+      "Value": "Loss",
+      "Description": "Indicates a person has left the scope of the application either by leaving the Navy or moving to an unsupported command."
+    },
+    {
+      "Id": "B3C550A0-E824-4B99-BA9B-0FDEA971D297",
+      "Value": "Second Party",
+      "Description": "Any second party members of the command."
+    },
+    {
+      "Id": "64ED0490-04D9-443A-BAE0-FD714623233A",
+      "Value": "TAD To Command",
+      "Description": "Indicates the member is TAD to the given command."
+    }
+  ]
+}
---
+++ CommandCentral/PreDefs/Def_WatchEligibilityGroups.cc
@@ -0,0 +1,11 @@
+﻿{
+  "TypeFullName": "CommandCentral.Entities.ReferenceLists.Watchbill.WatchEligibilityGroup",
+  "Definitions": [
+    {
+      "Id": "1F88A266-02BE-4E9B-ADBD-BE1D814B1CD8",
+      "Value": "Quarterdeck",
+      "Description": "This is the group of people who are eligible to stand the quarterdeck watch.",
+      "OwningChainOfCommand": "QuarterdeckWatchbill"
+    }
+  ]
+}
