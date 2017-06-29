8d5f61f255392d3296358c15e924f211f372ec21
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-06-28 23:34:49 -0400

    Work

--- CommandCentral.Test/WatchbillTests.cs
+++ CommandCentral.Test/WatchbillTests.cs
@@ -6,25 +6,6 @@ namespace CommandCentral.Test
     [TestClass]
     public class WatchbillTests
     {
-
-        [TestMethod]
-        public void CreateEligibilityGroup()
-        {
-            using (var session = DataAccess.DataProvider.CreateStatefulSession())
-            {
-                using (var transaction = session.BeginTransaction())
-                {
-
-
-
-                }
-            }
-        }
-
-        [TestMethod]
-        public void CreateWatchbill()
-        {
-            
-        }
+        
     }
 }
