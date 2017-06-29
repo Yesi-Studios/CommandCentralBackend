43acc29e8ccbbb22c9579cdf403e388edcdca29b
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-06-29 12:17:34 -0400

    settings

--- CommandCentral.Test/TestSettings.cs
+++ CommandCentral.Test/TestSettings.cs
@@ -11,10 +11,10 @@ namespace CommandCentral.Test
 
         public static string Database { get; } = "test_database";
         public static string Server { get; } = "localhost";
-        public static string Username { get; } = "root";
-        public static string Password { get; } = "password";
+        public static string Username { get; } = "anguslmm";
+        public static string Password { get; } = "applew";
         public static List<string> SMTPHosts { get; } = new List<string> { };
-        public static bool RebuildIfExists { get; } = true;
+        public static bool RebuildIfExists { get; } = false;
 
     }
 }
