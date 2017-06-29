910316f7760a4a01db71a62749eeffb140130822
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-06-28 23:49:40 -0400

    Added connection settings for tests.

--- CommandCentral.Test/DatabaseTests.cs
+++ CommandCentral.Test/DatabaseTests.cs
@@ -13,6 +13,21 @@ namespace CommandCentral.Test
         [TestMethod]
         public void SetupInMemoryDatabase()
         {
+
+            MySql.Server.MySqlServer server = MySql.Server.MySqlServer.Instance;
+
+            server.StartServer(3000);
+
+            var dbName = "commandcentraltestdatabase_" + AtwoodUtils.Utilities.RandomString(5);
+
+            DataAccess.DataProvider.InitializeAndRebuild(new MySql.Data.MySqlClient.MySqlConnectionStringBuilder(server.GetConnectionString(dbName)), dbName);
+
+            Assert.IsTrue(DataAccess.DataProvider.IsReady);
+        }
+
+        [TestMethod]
+        public void SetupRealDatabase()
+        {
             MySql.Server.MySqlServer server = MySql.Server.MySqlServer.Instance;
 
             server.StartServer(3000);
